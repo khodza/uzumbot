e709908d55a4b2d85e17df00c12c718dbc49af98
Izzattillakhodza <izzattillakhodza.m@payme.uz>
2024-02-17 20:13:27 +0500

    change req

--- src/uzum-api/api.ts
+++ src/uzum-api/api.ts
@@ -47,8 +47,20 @@ export class UzumApi {
         try {
             const response = await axios.get(url, {
                 headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Authorization": this.frontToken, // Assuming this.frontToken has the correct basic auth header value
                     "Accept": "application/json",
-                    "Authorization": this.authToken, // Use the authToken from signIn method
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "Accept-Language": "ru-RU",
+                    "Access-Content-Allow-Origin": "*",
+                    "Origin": "https://seller.uzum.uz",
+                    "Referer": "https://seller.uzum.uz/",
+                    "Sec-Ch-Ua": `"Not A Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"`,
+                    "Sec-Ch-Ua-Mobile": "?0",
+                    "Sec-Ch-Ua-Platform": `"Windows"`,
+                    "Sec-Fetch-Dest": "empty",
+                    "Sec-Fetch-Mode": "cors",
+                    "Sec-Fetch-Site": "same-site",
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
                 }
             });
